18d2181ba67ca767b4be6d100e06452793729461
Luinil <61002225+Flolink1er@users.noreply.github.com>
2025-12-16 17:45:43 +0100

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@ export default {
       gameFinished : false,
       apiCall : 'https://trouve-mot.fr/api/size/5',
       darkMode : localStorage.getItem('darkMode'),
-      wordToGuess: '',
+      wordToGuess: "",
+      keyboard: [],
+      attempts: [],
     }
   },
 
@@ -28,6 +30,7 @@ export default {
         .then(data => {
             console.log(data);
             this.wordToGuess = data[0]['name'];
+            localStorage.setItem('wordToGuess', this.wordToGuess);
         })
         .catch(error => {
             console.log(error.message);
@@ -36,18 +39,75 @@ export default {
       else{
         this.wordToGuess = localStorage.getItem('wordToGuess');
       }
-      localStorage.setItem('wordToGuess', this.wordToGuess);
-    }
+
+
+    },
+
+    //fonction qui vérifie si un clavier est déjà en localstorage, et dans le cas échéant le créer dans un état par défaut
+    setKeyboard(){
+      if (!localStorage.getItem(this.wordToGuess)){
+        this.keyboard = [
+          // Ligne 1
+          { key: "A", status: "unused" },
+          { key: "Z", status: "unused" },
+          { key: "E", status: "unused" },
+          { key: "R", status: "unused" },
+          { key: "T", status: "unused" },
+          { key: "Y", status: "unused" },
+          { key: "U", status: "unused" },
+          { key: "I", status: "unused" },
+          { key: "O", status: "unused" },
+          { key: "P", status: "unused" },
+
+          // Ligne 2
+          { key: "Q", status: "unused" },
+          { key: "S", status: "unused" },
+          { key: "D", status: "unused" },
+          { key: "F", status: "unused" },
+          { key: "G", status: "unused" },
+          { key: "H", status: "unused" },
+          { key: "J", status: "unused" },
+          { key: "K", status: "unused" },
+          { key: "L", status: "unused" },
+          { key: "M", status: "unused" },
+
+          // Ligne 3
+          { key: "ENTER", status: "unused" },
+          { key: "W", status: "unused" },
+          { key: "X", status: "unused" },
+          { key: "C", status: "unused" },
+          { key: "V", status: "unused" },
+          { key: "B", status: "unused" },
+          { key: "N", status: "unused" },
+          { key: "DEL", status: "unused" },
+        ];
+      }
+      else{
+        this.keyboard = JSON.parse(localStorage.getItem('keyboard'));
+      }
+      localStorage.setItem('keyboard', JSON.stringify(this.keyboard));
+    },
+
+    //fonction qui vérifie si des tentatives sont déjà présentes
+    setAttempts(){
+      if (localStorage.getItem('attempts')){
+        this.attempts = localStorage.getItem('attempts');
+      }
+    },
+
   },
 
-  beforeMount(){
-    this.setWord(); //lance la fonction au chargement de la page
+  beforeMount(){ //lance des fonctions au chargement de la page
+    this.setWord();
+    this.setKeyboard();
+    this.setAttempts();
   }
 };
 </script>
 
 <template>
+  <button v-on:click="test">TEST</button>
   <WordGrid></WordGrid>
-  <KeyInput></KeyInput>
+  <KeyInput :keyboard="keyboard"></KeyInput>
   <ResultPopout v-if="gameFinished"></ResultPopout>
 </template>
